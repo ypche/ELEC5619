3cc90fd4e8b1b1855483478241c27833261149f3
Yingping Che <ypche29@gmail.com>
2018-10-04 11:44:45 +1000

    Add OrderDao

---
+++ src/main/java/au/usyd/onlineshopping/dao/OrderDao.java
@@ -0,0 +1,13 @@
+package au.usyd.onlineshopping.dao;
+
+import java.util.List;
+
+import au.usyd.onlineshopping.Entity.Order;
+import au.usyd.onlineshopping.Entity.User;
+
+public interface OrderDao {
+
+	public List<Order> getAllOrders();
+	public Order getOrderById(long id);
+	public Order getOrderByUser(User user);
+}
