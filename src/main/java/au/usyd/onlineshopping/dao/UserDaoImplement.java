317d219f6e6124fc06e9f0cddf0d98bbda149e1d
lznelson <zliu0098@uni.sydney.edu.au>
2018-10-28 09:47:52 +1100

    Update User Entity

--- src/main/java/au/usyd/onlineshopping/dao/UserDaoImplement.java
+++ src/main/java/au/usyd/onlineshopping/dao/UserDaoImplement.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import au.usyd.onlineshopping.Entity.User;
-
+// UserDaoImplement
 @Repository
 public class UserDaoImplement implements UserDao {
 
