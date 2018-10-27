2af7adffe45fb8db3470bc3da76a31f0bc363329
lznelson <zliu0098@uni.sydney.edu.au>
2018-10-28 10:08:25 +1100

    Modify user login function

--- src/main/java/au/usyd/onlineshopping/dao/UserDaoImplement.java
+++ src/main/java/au/usyd/onlineshopping/dao/UserDaoImplement.java
@@ -12,7 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import au.usyd.onlineshopping.Entity.User;
-// UserDaoImplement
+// UserDaoImplement login
+
 @Repository
 public class UserDaoImplement implements UserDao {
 
